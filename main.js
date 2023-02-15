42e0c47db0bf8e0e24bdc5f143b0480d3efecdda
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-15 16:13:41 +0530

    divisible by 11 added.

---
+++ main.js
@@ -0,0 +1,11 @@
+	
+let number = Number(prompt("Enter any number"));
+
+if(number%11 == 0){
+     document.write(`${number} is divisible by 11.`);
+ }
+ else{
+     document.write(`${number} is doesn't divisible by 11.`);
+	}
+	
+	
